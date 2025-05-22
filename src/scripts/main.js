5fc31b335b687df682fb2089b8e7582ef6666169
Filip-Groh <filip.groh.023@pslib.cz>
2025-05-23 00:26:13 +0200

    cookies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,29 @@
-import "./hamburger"
-import "./swiper"
+import "./hamburger";
+import "./swiper";
 
-console.log("Hello world!")
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/";
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector("#darkmode-switch").checked = getCookie("invertedmode") === "true";
+})
+
+document.querySelector("#darkmode-switch").addEventListener("change", (e) => {
+    setCookie("invertedmode", e.target.checked);
+})
